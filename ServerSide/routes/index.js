6ef9e3d244d6d130d51b1c1d9f2d08276ce88262
Mohamed_Ibrahim_199 <mohamed1989877@gmail.com>
2021-01-29 19:04:35 +0200

    removed server side folder

--- ServerSide/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-let user = require('../controllers/user')
-
-router.get('/Login', user.Login)
-router.post('/SignUp', user.SignUp)
-
-module.exports = router;
