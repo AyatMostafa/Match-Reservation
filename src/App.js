e58570bfba1ddb265dbdbcb32b76f49f367173e7
ahmed <engahmedhamdy1212@gmail.com>
2021-01-31 17:06:39 +0200

    add customer's frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Home from './components/HomeComponent.js';
 import Customer from './components/CustomerComponent.js'
+import ReserveSeat from './components/reserveSeat'
+import CancelReservation from './components/CancelReservation'
 import { BrowserRouter, Route,Switch } from 'react-router-dom';
 import './App.css';
+import Square from './components/Square'
+
 
 function App() {
   return (
+
     <BrowserRouter>
          {/*  <div className="App">
             <Home /> 
@@ -13,6 +18,18 @@ function App() {
           <Switch>
           <Route path="/profile">
             <Customer />
+          </Route>
+
+          <Route path="/showSeats">
+            <Square />
+          </Route>
+
+          <Route path="/reserve">
+            <ReserveSeat />
+          </Route>
+
+          <Route path="/cancel">
+            <CancelReservation />
           </Route>
         
           <Route path="/">
@@ -25,3 +42,20 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// function App() {
+//   return (
+//     <MainStage
+//       onSelectSeat={seatId => {
+//         console.log("selected - " + seatId);
+//       }}
+//     />
+//   );
+// }
+
+// export default App;
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
